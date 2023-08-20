6c29eef0f42b4080a003e547b34667906edaeccf
sagarpise0202 <sagarpise0202@gmail.com>
2023-08-20 20:31:40 +0530

    Initial first commit

---
+++ src/test/java/com/gsuare/dummy/erp/DummyGsquare1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsuare.dummy.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyGsquare1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
